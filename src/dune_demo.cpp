41aa7a2e8084fa6659b3d98a24d0c1bd01c9f4b7
Eugene <spring_86@mail.ru>
2024-12-22 20:30:30 +0300

    Remove unused code

--- src/dune_demo.cpp
+++ src/dune_demo.cpp
@@ -12,35 +12,8 @@
 #include "Vertex.hpp"
 #include "ShaderProgram.hpp"
 
-static float lastX = 1200 / 2.0f;
-static float lastY = 800 / 2.0f;
-static float pitch = 20;
-static float yaw = 180;
-
-static void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
-    GLfloat xoffset = xpos - lastX;
-    GLfloat yoffset = lastY - ypos;
-    lastX = xpos;
-    lastY = ypos;
-
-    GLfloat sensitivity = 0.05f;
-    xoffset *= sensitivity;
-    yoffset *= sensitivity;
-
-    yaw   -= xoffset;
-    pitch += yoffset;
-
-    if(pitch > 89.0f)
-    pitch =  89.0f;
-    if(pitch < -89.0f)
-    pitch = -89.0f;
-}
-
 void dune_demo(GLFWwindow* window, GLint scr_width, GLint scr_height)
 {
-    glfwSetCursorPosCallback(window, mouse_callback);
-
     auto is_key_pressed = [window](int32_t key)
     {
         return glfwGetKey(window, key) == GLFW_PRESS;
@@ -71,8 +44,8 @@ void dune_demo(GLFWwindow* window, GLint scr_width, GLint scr_height)
 
     std::array<Vertex, 4> vertices = 
     {
-        Vertex(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec2(0.0f, 0.0f)),
-        Vertex(glm::vec3(1200.0f, 0.0f, 0.0f), glm::vec2(1.0f, 0.0f)),
+        Vertex(glm::vec3(0.0f, 0.0f, 0.0f),      glm::vec2(0.0f, 0.0f)),
+        Vertex(glm::vec3(1200.0f, 0.0f, 0.0f),   glm::vec2(1.0f, 0.0f)),
         Vertex(glm::vec3(1200.0f, 800.0f, 0.0f), glm::vec2(1.0f, 1.0f)),
         Vertex(glm::vec3(0.0f,    800.0f, 0.0f), glm::vec2(0.0f, 1.0f))
     };
@@ -117,16 +90,7 @@ void dune_demo(GLFWwindow* window, GLint scr_width, GLint scr_height)
             continue;
         }
 
-        float radians = glm::radians(yaw);
-        float speed = 0.0f;
-
-        if (is_key_pressed(GLFW_KEY_W))   speed = -1.1f;
-        if (is_key_pressed(GLFW_KEY_S))   speed =  1.1f;
-        if (is_key_pressed(GLFW_KEY_A)) { speed =  1.1f; radians -= M_PI_2; }
-        if (is_key_pressed(GLFW_KEY_D)) { speed =  1.1f; radians += M_PI_2; }
-
         glm::mat4 model_view = glm::identity<glm::mat4>();
-        //model_view = glm::translate(model_view, glm::vec3(-1, -1, 0));
 
         auto MVP = projection * model_view;
         glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, glm::value_ptr(MVP));
