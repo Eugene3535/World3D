ce01097d59cb2a6875dd0a818e22bacb2469a1f2
Eugene <spring_86@mail.ru>
2025-01-02 16:30:50 +0300

    Added index buffer wrapper

---
+++ renderlib/src/IndexBuffer.cpp
@@ -0,0 +1,59 @@
+#include "IndexBuffer.hpp"
+#include <glad/glad.h>
+
+IndexBuffer::IndexBuffer(const void* data, uint32_t count, VertexBuffer::Usage usage): 
+    m_count(count)
+{
+    auto usageToGLenum = [](const VertexBuffer::Usage usage) -> int32_t
+    {
+        switch (usage)
+        {
+            case VertexBuffer::Usage::Static:  return GL_STATIC_DRAW;
+            case VertexBuffer::Usage::Dynamic: return GL_DYNAMIC_DRAW;
+            case VertexBuffer::Usage::Stream:  return GL_STREAM_DRAW;
+        }
+        return GL_STREAM_DRAW;
+    };
+
+    glGenBuffers(1, &m_handle);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_handle);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(uint32_t), data, usageToGLenum(usage));
+}
+
+
+IndexBuffer::IndexBuffer(IndexBuffer&& indexBuffer) noexcept: 
+    m_handle(indexBuffer.m_handle),
+    m_count(indexBuffer.m_count)
+{
+    indexBuffer.m_handle = 0;
+    indexBuffer.m_count = 0;
+}
+
+
+IndexBuffer& IndexBuffer::operator = (IndexBuffer&& indexBuffer) noexcept
+{
+    m_handle = indexBuffer.m_handle;
+    m_count = indexBuffer.m_count;
+    indexBuffer.m_handle = 0;
+    indexBuffer.m_count = 0;
+
+    return *this;
+}
+
+
+IndexBuffer::~IndexBuffer() noexcept
+{
+    glDeleteBuffers(1, &m_handle);
+}
+
+
+void IndexBuffer::bind(IndexBuffer* buffer) noexcept
+{
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer ? buffer->m_handle : 0);
+}
+
+
+uint32_t IndexBuffer::getCount() const noexcept
+{
+    return m_count;
+}
---
+++ renderlib/src/IndexBuffer.hpp
@@ -0,0 +1,24 @@
+#ifndef INDEX_BUFFER_HPP
+#define INDEX_BUFFER_HPP
+
+#include "VertexBuffer.hpp"
+
+class IndexBuffer 
+{
+public:
+    IndexBuffer(const void* data, uint32_t count, VertexBuffer::Usage usage = VertexBuffer::Usage::Static);
+    IndexBuffer(const IndexBuffer&) = delete;
+    IndexBuffer(IndexBuffer&& index_buffer) noexcept;
+    IndexBuffer& operator = (const IndexBuffer&) = delete;
+    IndexBuffer& operator = (IndexBuffer&& index_buffer) noexcept;
+    ~IndexBuffer() noexcept;
+
+    static void bind(IndexBuffer* buffer) noexcept;
+    uint32_t getCount() const noexcept;
+
+private:
+    uint32_t m_handle;
+    uint32_t m_count;
+};
+
+#endif // !INDEX_BUFFER_HPP
