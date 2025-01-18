f7e2176b61477aafe73b80a4cf23c298dbdaf445
Eugene <spring_86@mail.ru>
2025-01-18 18:47:13 +0300

    GlBuffer functional

--- render_engine/src/gl_resources/buffers/GlBuffer.cpp
+++ render_engine/src/gl_resources/buffers/GlBuffer.cpp
@@ -5,48 +5,52 @@
 #include "gl_resources/buffers/GlBuffer.hpp"
 
 
-static constexpr GLenum targetToGlenum(GlBuffer::Target target) noexcept
+GlBuffer* GlBuffer::m_instance;
+
+
+GlBuffer::GlBuffer() noexcept:
+    genBuffers(nullptr),
+    delBuffers(nullptr)
 {
-    switch (target)
-    {
-        case GlBuffer::Array:             return GL_ARRAY_BUFFER;
-        case GlBuffer::AtomicCounter:     return GL_ATOMIC_COUNTER_BUFFER;
-        case GlBuffer::CopyRead:          return GL_COPY_READ_BUFFER;
-        case GlBuffer::CopyWrite:         return GL_COPY_WRITE_BUFFER;
-        case GlBuffer::DispatchIndirect:  return GL_DISPATCH_INDIRECT_BUFFER;
-        case GlBuffer::DrawIndirect:      return GL_DRAW_INDIRECT_BUFFER;
-        case GlBuffer::ElementArray:      return GL_ELEMENT_ARRAY_BUFFER;
-        case GlBuffer::PixelPack:         return GL_PIXEL_PACK_BUFFER;
-        case GlBuffer::PixelUnpack:       return GL_PIXEL_UNPACK_BUFFER;
-        case GlBuffer::Query:             return GL_QUERY_BUFFER;
-        case GlBuffer::ShaderStorage:     return GL_TEXTURE_BUFFER;
-        case GlBuffer::Texture:           return GL_SHADER_STORAGE_BUFFER;
-        case GlBuffer::TransformFeedback: return GL_TRANSFORM_FEEDBACK_BUFFER;
-        case GlBuffer::Uniform:           return GL_UNIFORM_BUFFER;    
-    
-        default: return GL_ARRAY_BUFFER;
-    }
-}
-
-
-std::vector<uint32_t> GlBuffer::m_buffers;
-GlBuffer* GlBuffer::s_instance;
-
-
-GlBuffer::GlBuffer() noexcept
-{
-    assert(s_instance == nullptr);
-    s_instance = this;
+    assert(m_instance == nullptr);
+    m_instance = this;
+    genBuffers = glGenBuffers;
+    delBuffers = glDeleteBuffers;
 }
 
 
 GlBuffer::~GlBuffer() noexcept
 {
-
+    if(!m_instance->m_buffers.empty())
+        glDeleteBuffers(static_cast<GLsizei>(m_buffers.size()), m_buffers.data());
 }
 
 
 void GlBuffer::bind(GlBuffer::Target target, uint32_t buffer) noexcept
 {
+    auto targetToGlenum = [](GlBuffer::Target target) noexcept -> GLenum
+    {
+        switch (target)
+        {
+            case GlBuffer::Array:             return GL_ARRAY_BUFFER;
+            case GlBuffer::AtomicCounter:     return GL_ATOMIC_COUNTER_BUFFER;
+            case GlBuffer::CopyRead:          return GL_COPY_READ_BUFFER;
+            case GlBuffer::CopyWrite:         return GL_COPY_WRITE_BUFFER;
+            case GlBuffer::DispatchIndirect:  return GL_DISPATCH_INDIRECT_BUFFER;
+            case GlBuffer::DrawIndirect:      return GL_DRAW_INDIRECT_BUFFER;
+            case GlBuffer::ElementArray:      return GL_ELEMENT_ARRAY_BUFFER;
+            case GlBuffer::PixelPack:         return GL_PIXEL_PACK_BUFFER;
+            case GlBuffer::PixelUnpack:       return GL_PIXEL_UNPACK_BUFFER;
+            case GlBuffer::Query:             return GL_QUERY_BUFFER;
+            case GlBuffer::ShaderStorage:     return GL_TEXTURE_BUFFER;
+            case GlBuffer::Texture:           return GL_SHADER_STORAGE_BUFFER;
+            case GlBuffer::TransformFeedback: return GL_TRANSFORM_FEEDBACK_BUFFER;
+            case GlBuffer::Uniform:           return GL_UNIFORM_BUFFER;    
+        
+            default: return GL_ARRAY_BUFFER;
+        }
+    };
+
+    assert(m_instance);
     glBindBuffer(targetToGlenum(target), buffer);
 }
--- render_engine/src/gl_resources/buffers/GlBuffer.hpp
+++ render_engine/src/gl_resources/buffers/GlBuffer.hpp
@@ -1,9 +1,13 @@
 #ifndef GL_BUFFER_HPP
 #define GL_BUFFER_HPP
 
+#include <array>
 #include <vector>
 
-class GlBuffer
+#include "Export.hpp"
+
+
+class RE_API GlBuffer final
 {
 public:
     enum Target: uint32_t
@@ -32,13 +36,28 @@ public:
     };
 
     GlBuffer() noexcept;
+    GlBuffer(const GlBuffer&) noexcept = delete;
+    GlBuffer(GlBuffer&&) noexcept = delete;
+    GlBuffer& operator = (const GlBuffer&) noexcept = delete;
+    GlBuffer& operator = (GlBuffer&&) noexcept = delete;
     ~GlBuffer() noexcept;
+
+    template<size_t N>
+    static void deleteBuffers(const std::array<uint32_t, N>& buffers) noexcept;
+
+    template<size_t N>
+    static std::array<uint32_t, N> generateBuffers() noexcept;
 
     static void bind(Target target, uint32_t buffer) noexcept;
 
 private:
-    static std::vector<uint32_t> m_buffers;
-    static GlBuffer* s_instance;
+    std::vector<uint32_t> m_buffers;
+    static GlBuffer* m_instance;
+
+    void(*genBuffers)(int32_t, uint32_t*);
+    void(*delBuffers)(int32_t, const uint32_t*);
 };
+
+#include "gl_resources/buffers/GlBuffer.inl"
 
 #endif // !GL_BUFFER_HPP
